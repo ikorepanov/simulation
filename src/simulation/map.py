589d4a64362aa462bc433ce604582aeea21683db
ikorepanov <ikorepanov@gmail.com>
2025-01-21 09:38:56 +0700

    Add mypy

--- src/simulation/map.py
+++ src/simulation/map.py
@@ -6,9 +6,9 @@ class Map():
     def __init__(self, coordinates: Coordinates, entity: Entity):
         self.some = {coordinates: entity}
 
-    def set_entites(self, coordinates, entity):
+    def set_entites(self, coordinates: Coordinates, entity: Entity) -> None:
         entity.coordinates = coordinates
         ...
 
-    def setup_start_entity_positions(self):  # Первоначальная расстановка сущностей по карте
+    def setup_start_entity_positions(self) -> None:  # Первоначальная расстановка сущностей по карте
         pass
