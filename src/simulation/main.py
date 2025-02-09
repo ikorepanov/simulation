c3c190030b81e9705b4aded23a7af3cdd31a8334
ikorepanov <ikorepanov@gmail.com>
2025-02-09 12:24:07 +0700

    Add main func

--- src/simulation/main.py
+++ src/simulation/main.py
@@ -1,5 +1,11 @@
 from simulation.game import Game
 
-game = Game()
-game.show_start_screen()
-game.run()
+
+def main() -> None:
+    game = Game()
+    game.show_start_screen()
+    game.run()
+
+
+if __name__ == '__main__':
+    main()
