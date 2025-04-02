b4b397b9a6363a9ea16fdaf91e34168a036d7ba7
ikorepanov <ikorepanov@gmail.com>
2025-04-02 10:25:11 +0700

    Add Entity class

--- src/simulation/entity.py
+++ src/simulation/entity.py
@@ -1,8 +1,23 @@
 from abc import ABC
 
-from simulation.coordinates import Coordinates
+import pygame as pg
+from pygame.sprite import Sprite
+
+from simulation.settings import GREEN
 
 
-class Entity(ABC):
-    def __init__(self, coordinates: Coordinates):
-        self.coordinates = coordinates
+class Entity(ABC, Sprite):
+    def __init__(
+        self,
+        x: int,
+        y: int,
+        w: int,
+        h: int,
+    ):
+        super().__init__()
+
+        self.image = pg.Surface((w, h))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
