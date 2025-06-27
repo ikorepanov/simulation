a47d7afd0cc8c4c5d0bba2d128c6edf62312ec51
ikorepanov <ikorepanov@gmail.com>
2025-06-27 10:46:32 +0700

    Update Predator, Herbivore

--- src/simulation/herbivore.py
+++ src/simulation/herbivore.py
@@ -3,10 +3,14 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from simulation.creature import Creature
-from simulation.settings import HERBIVORE_SPEED, TILESIZE, VORTEX
+from simulation.settings import HERBIVORE_HP, HERBIVORE_SPEED, TILESIZE, VORTEX
 
 if TYPE_CHECKING:
     from simulation.map import Map
+
+from typing import Any
+
+from pygame.sprite import AbstractGroup
 
 from simulation.coordinate import Coordinate
 from simulation.grass import Grass
@@ -18,8 +22,15 @@ class Herbivore(Creature):
         map: Map,
         color: tuple[int, int, int] = VORTEX,
         speed: int = HERBIVORE_SPEED,
+        hp: int = HERBIVORE_HP,
+        class_specific_groups: tuple[AbstractGroup[Any], ...] | None = None,
     ):
-        super().__init__(map, color, speed, (map.game.herbivores,))
+        if class_specific_groups is None:
+            class_specific_groups = (
+                map.game.herbivores,
+            )
+
+        super().__init__(map, color, speed, hp, class_specific_groups)
 
         if self.map.game.development_mode:
             x = 0
@@ -30,13 +41,16 @@ class Herbivore(Creature):
 
         self.prey = Grass
 
-    def make_move(self, target_coordinate: Coordinate) -> None:
+    def make_move(self) -> None:
+        if self.state == 'moving':
+            self.move_towards()
+        if self.state == 'eating':
+            self.eat_grass()
+
+    def move_towards(self) -> None:
         pass
 
-    def search(self) -> None:
-        pass
-
-    def eat(self) -> None:
+    def eat_grass(self) -> None:
         pass
 
     def loose_hp(self) -> None:
--- src/simulation/predator.py
+++ src/simulation/predator.py
@@ -3,12 +3,16 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from simulation.creature import Creature
-from simulation.settings import ATTACK_POWER, PREDATOR_SPEED, RED
+from simulation.settings import ATTACK_POWER, PREDATOR_HP, PREDATOR_SPEED, RED
 
 if TYPE_CHECKING:
     from simulation.map import Map
 
-from simulation.coordinate import Coordinate
+from typing import Any
+
+from pygame.sprite import AbstractGroup
+
+from simulation.herbivore import Herbivore
 
 
 class Predator(Creature):
@@ -17,17 +21,28 @@ class Predator(Creature):
         map: Map,
         color: tuple[int, int, int] = RED,
         speed: int = PREDATOR_SPEED,
+        hp: int = PREDATOR_HP,
         attack_power: int = ATTACK_POWER,
+        class_specific_groups: tuple[AbstractGroup[Any], ...] | None = None,
     ):
-        super().__init__(map, color, speed, (map.game.predators,))
+        if class_specific_groups is None:
+            class_specific_groups = (
+                map.game.predators,
+            )
 
+        super().__init__(map, color, speed, hp, class_specific_groups)
+
+        self.prey = Herbivore
         self.attack_power = attack_power
 
-    def make_move(self, target_coordinate: Coordinate) -> None:
+    def make_move(self) -> None:
+        if self.state == 'moving':
+            self.move_towards()
+        if self.state == 'eating':
+            self.attack_prey()
+
+    def move_towards(self) -> None:
         pass
 
-    def chase(self) -> None:
-        pass
-
-    def attack(self) -> None:
+    def attack_prey(self) -> None:
         pass
