ea8c9bad91b8996721cca243b050699611979d2e
ikorepanov <ikorepanov@gmail.com>
2025-11-25 11:42:29 +0700

    Move color_schemes from main

--- src/simulation/main.py
+++ src/simulation/main.py
@@ -1,7 +1,7 @@
 import sys
 from simulation.action import Action, MoveAction, PlaceEntitiesAction
 from simulation.game_map import Map
-from simulation.renderer.color_schemes import ColorScheme
+from simulation.renderer.color_schemes import color_schemes
 from simulation.renderer.consolerenderer import ConsoleRenderer
 from simulation.settings import COLOR_SCHEME
 from simulation.simulation import Simulation
@@ -11,16 +11,7 @@ from simulation.entity_creator import EntityCreator
 def main() -> None:
     game_map = Map()
 
-    color_scheme = {
-        'industrial': ColorScheme(8, 7),
-        'midnight': ColorScheme(18, 27),
-        'ocean': ColorScheme(4, 12),
-        'forest': ColorScheme(2, 10),
-        'twilight': ColorScheme(5, 13),
-        'glacier': ColorScheme(30, 51),
-        'abyss': ColorScheme(23, 66),
-    }
-    renderer = ConsoleRenderer(color_scheme[COLOR_SCHEME])
+    renderer = ConsoleRenderer(color_schemes[COLOR_SCHEME])
 
     entity_creator = EntityCreator()
 
--- src/simulation/renderer/color_schemes.py
+++ src/simulation/renderer/color_schemes.py
@@ -5,3 +5,14 @@ from dataclasses import dataclass
 class ColorScheme:
     bg_dark: int
     bg_light: int
+
+
+color_schemes = {
+    'industrial': ColorScheme(8, 7),
+    'midnight': ColorScheme(18, 27),
+    'ocean': ColorScheme(4, 12),
+    'forest': ColorScheme(2, 10),
+    'twilight': ColorScheme(5, 13),
+    'glacier': ColorScheme(30, 51),
+    'abyss': ColorScheme(23, 66),
+}
