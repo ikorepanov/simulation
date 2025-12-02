5aaebcfe4944318aa941034fd2da74ccf3f75465
ikorepanov <ikorepanov@gmail.com>
2025-12-02 20:02:10 +0700

    Clean Coordinate, remove CoordinateShift

--- src/simulation/coordinate.py
+++ src/simulation/coordinate.py
@@ -1,36 +1,14 @@
-
-from __future__ import annotations
-
-from simulation.coordinate_shift import CoordinateShift
-from simulation.settings import HEIGHT, TILESIZE, WIDTH
-
-
 class Coordinate:
     def __init__(
         self,
         x: int,
         y: int,
     ):
-        self.x = x  # relative units
-        self.y = y  # relative units
+        self.x = x
+        self.y = y
 
     def __repr__(self) -> str:
         return f'Coordinate {id(self)}'
-
-    def shift(self, shift: CoordinateShift) -> Coordinate:
-        return Coordinate(self.x + shift.x_shift, self.y + shift.y_shift)
-
-    def can_shift(self, shift: CoordinateShift) -> bool:
-        x = self.x + shift.x_shift
-        y = self.y + shift.y_shift
-
-        if x < 0 or x > (WIDTH - TILESIZE) / TILESIZE:
-            return False
-
-        if y < 0 or y > (HEIGHT - TILESIZE) / TILESIZE:
-            return False
-
-        return True
 
     def __hash__(self) -> int:
         return hash((self.x, self.y))
